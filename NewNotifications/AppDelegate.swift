02ad44e629cba8ab5185be28e86f833ac3e08ca8
Joe Kim <josephkim0224@gmail.com>
2017-01-28 05:26:28 -0500

    Add some actions to local user notification

--- NewNotifications/AppDelegate.swift
+++ NewNotifications/AppDelegate.swift
@@ -48,7 +48,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     private func configureUserNotification() {
-        let category = UNNotificationCategory(identifier: "myNotificationCategory", actions: [], intentIdentifiers: [], options: [])
+        let favAction = UNNotificationAction(identifier: "fistBump", title: "👊 Fist Bump", options: [])
+        let dismissAction = UNNotificationAction(identifier: "dismiss", title: "Dismiss", options: [])
+        
+        
+        
+        let category = UNNotificationCategory(identifier: "myNotificationCategory", actions: [favAction, dismissAction], intentIdentifiers: [], options: [])
         UNUserNotificationCenter.current().setNotificationCategories([category])
     }
 }
@@ -56,6 +61,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler(.alert)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    
+        print("Response received for \(response.actionIdentifier)")
+        completionHandler()
     }
 }
 
